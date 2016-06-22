7880082127f942e6b10a05f2205057daf86e983e
Alejandro Nanez <alejonanez@gmail.com>
2016-06-22 18:16:18 -0500

    feat: Updated index and added basic styles

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 /**
  * Base file for all your application.
  */
+require('styles/style');
 console.log('========================');
 console.log('ES6 PROJECT BOILERPLATE');
 console.log('UP AND RUNNING!');
