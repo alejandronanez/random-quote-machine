7ef30ec700e95789b8763feebe278ea35eac198b
Alejandro Nanez <alejonanez@gmail.com>
2016-06-22 20:00:03 -0500

    feat(Basic styling):

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 /* eslint no-console: "off" */
-/**
- * Base file for all your application.
- */
+import 'styles/style.css';
 console.log('========================');
 console.log('ES6 PROJECT BOILERPLATE');
 console.log('UP AND RUNNING!');
